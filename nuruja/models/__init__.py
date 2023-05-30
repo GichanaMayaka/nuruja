cc8b042d755ef7dae967c72214703cd8a9dd134a
Gichana <gichanamayaka@gmail.com>
2023-05-30 17:57:32 +0300

    adjusted relationships to reflect application schema

--- nuruja/models/__init__.py
+++ nuruja/models/__init__.py
@@ -55,6 +55,8 @@ class User(db.Model, CRUDMixin):
     address = db.Column(db.String(100), nullable=False, unique=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
+    borrowing = db.relationship("Borrowing", back_populates="user")
+
     def __init__(self, username: str, email: str, phone_number: str, address: str, is_admin: bool = False) -> None:
         self.username = username
         self.email = email
@@ -73,10 +75,15 @@ class Book(db.Model, CRUDMixin):
     author = db.Column(db.String(120), nullable=False, unique=False)
     isbn = db.Column(db.String(120), nullable=False, unique=True)
     date_of_publication = db.Column(db.DateTime, nullable=False, unique=False, default=pendulum.now)
-
-    # TODO: Implement categories as a relationship
-    category = db.relationship("Category")
     status = db.Column(db.String(15), nullable=False, unique=False, default="not-rented")
+
+    # Foreign Key[s]
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+
+    # Relationship[s]
+    category = db.relationship("Category", back_populates="book")
+    borrowing = db.relationship("Borrowing", back_populates="book")
+    shelf = db.relationship("Shelf", back_populates="book")
 
     def __init__(self, title: str, author: str, isbn: str, date_of_publication: pendulum.DateTime,
                  category: str, status: str = "not-rented") -> None:
@@ -97,6 +104,9 @@ class Category(db.Model, CRUDMixin):
     name = db.Column(db.String(20), nullable=False, unique=True)
     description = db.Column(db.String(200), nullable=True, unique=False)
 
+    # Relationship[s]
+    book = db.relationship("Book", back_populates="Category")
+
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
@@ -109,7 +119,12 @@ class Shelf(db.Model, CRUDMixin):
     __tablename__ = "shelf"
 
     floor = db.Column(db.String(10), nullable=True, unique=True)
-    book_id = db.relationship("Book")
+
+    # Foreign Key[s]
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"))
+
+    # Relationship[s]
+    book = db.relationship("Book", back_populates="shelf")
 
     def __init__(self, floor: str):
         self.floor = floor
@@ -121,10 +136,16 @@ class Shelf(db.Model, CRUDMixin):
 class Borrowing(db.Model, CRUDMixin):
     __tablename__ = "borrowing"
 
-    user_id = db.relationship("User")
-    book_id = db.relationship("Book")
     rent_fee = db.Column(db.Float, default=100, nullable=False, unique=False)
     is_return = db.Column(db.Boolean, default=False, nullable=False, unique=False)
+
+    # Foreign Key[s]
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"))
+
+    # Relationship[s]
+    book = db.relationship("Book", back_populates="borrowing")
+    user = db.relationship("User", back_populates="borrowing")
 
 # class Order(db.Model, CRUDMixin):
 # class Transaction(db.Model, CRUDMixin)
